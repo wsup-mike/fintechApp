eb46e86bd9303397d3e8285010921dbd337064f7
Mike Ramos <michaelangeloram8@gmail.com>
2024-04-30 20:59:49 -0500

    next commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,10 +9,10 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+// export const unstable_settings = {
+//   // Ensure that reloading on `/modal` keeps a back button present.
+//   initialRouteName: '(tabs)',
+// };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -44,6 +44,7 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
       <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }}/>
       </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import { useAssets } from 'expo-asset';
+import { Video } from 'expo-av';
 
 const Page = () => {
+  const [assets] = useAssets([require('@/assets/videos/intro.mp4')]);
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={styles.container}>
+      { assets && 
+        <Video source={{ uri: assets[0].uri }} style={{ width: 300, height: 300 }}/>
+      }
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  }
+});
 
 export default Page;
