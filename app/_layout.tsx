23ac18b4055887fbc97bdc4e3d2a8e5606e56798
Mike Ramos <michaelangeloram8@gmail.com>
2024-05-02 09:46:35 -0500

    New commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,29 +2,28 @@ import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
-// export const unstable_settings = {
-//   // Ensure that reloading on `/modal` keeps a back button present.
-//   initialRouteName: '(tabs)',
-// };
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
+
+  const router = useRouter();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -41,28 +40,61 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
   return (
-      <Stack>
-        <Stack.Screen name='index' options={{ headerShown: false }}/>
-        <Stack.Screen name='signup' options={{ 
-          title: '',
-          headerBackTitle: '',
-          headerStyle: { backgroundColor: Colors.background },
-          headerLeft: () => (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }}/>
+      <Stack.Screen name='signup' options={{ 
+        title: '',
+        headerBackTitle: '',
+        headerStyle: { backgroundColor: Colors.background },
+        headerLeft: () => (
+          <TouchableOpacity onPress={router.back}>
+            <Ionicons 
+              name='arrow-back'
+              size={34}
+              color={Colors.primary}
+            />
+          </TouchableOpacity>
+        ),
+        }}
+      />
+      <Stack.Screen name='login' options={{ 
+        title: '',
+        headerBackTitle: '',
+        headerStyle: { backgroundColor: Colors.background },
+        headerLeft: () => (
+          <TouchableOpacity onPress={router.back}>
+            <Ionicons 
+              name='arrow-back'
+              size={34}
+              color={Colors.primary}
+            />
+          </TouchableOpacity>
+        ),
+        headerRight: () => (
+          <Link href={'/help'} asChild>
             <TouchableOpacity>
               <Ionicons 
-                name='arrow-back'
+                name='help-circle-outline'
                 size={34}
                 color={Colors.primary}
               />
             </TouchableOpacity>
-          ),
-          }}
-        />
-      </Stack>
+          </Link>
+        ),
+        }}
+      />
+  </Stack>
+);
+}
+
+const RootLayoutNav = () => {
+  return (
+    <>
+      <StatusBar style='light' />
+      <InitialLayout />
+    </>
   );
 }
+
+export default RootLayoutNav;
--- app/login.tsx
+++ app/login.tsx
@@ -1,12 +1,73 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import { Stack, useRouter } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 
-const Page = () => {
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  const router = useRouter();
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
-  )
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }}/>
+      <Stack.Screen name='signup' options={{ 
+        title: '',
+        headerBackTitle: '',
+        headerStyle: { backgroundColor: Colors.background },
+        headerLeft: () => (
+          <TouchableOpacity onPress={router.back}>
+            <Ionicons 
+              name='arrow-back'
+              size={34}
+              color={Colors.primary}
+            />
+          </TouchableOpacity>
+        ),
+        }}
+      />
+  </Stack>
+);
 }
 
-export default Page
+const RootLayoutNav = () => {
+  return (
+    <>
+      <StatusBar style='light' />
+      <InitialLayout />
+    </>
+  );
+}
+
+export default RootLayoutNav;
