299dfc922c86b0fc8c20fd41e13d8205ab37f239
Mike Ramos <michaelangeloram8@gmail.com>
2024-05-06 10:36:03 -0500

    next commit

--- app/signup.tsx
+++ app/signup.tsx
@@ -6,9 +6,7 @@ import { Link, useRouter } from 'expo-router';
 import { KeyboardAvoidingView } from 'react-native';
 import { useSignUp } from '@clerk/clerk-expo';
 
-
 const Page = () => {
-
   const [countryCode, setCountryCode] = useState('+1');
   const [phoneNumber, setPhoneNumber] = useState('');
   const keyboardVerticalOffset = Platform.OS === 'ios' ? 90 : 0;
@@ -19,86 +17,86 @@ const Page = () => {
     const fullPhoneNumber = `${countryCode}${phoneNumber}`;
     router.push({ pathname: '/verify/[phone]', params: { phone: fullPhoneNumber } });
 
-    //   try {
-    //     await signUp!.create({
-    //       phoneNumber: fullPhoneNumber
-    //     });
+    // try {
+    //   await signUp!.create({
+    //     phoneNumber: fullPhoneNumber
+    //   });
 
-    //     router.push({ pathname: '/verify/[phone]', params: { phone: fullPhoneNumber } });
-    //   } catch (error) {
-    //     console.error('Error signing up:', error);
-    //   }
-    // };
-
-    return (
-      <KeyboardAvoidingView style={{ flex: 1 }} behavior='padding' keyboardVerticalOffset={keyboardVerticalOffset}>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={defaultStyles.container}>
-            <Text style={defaultStyles.header}>Let's get started!</Text>
-            <Text style={defaultStyles.descriptionText}>Enter your phone number to start. We will send you confirmation code shortly!</Text>
-            <View style={styles.inputContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder='Country Code'
-                placeholderTextColor={Colors.dark}
-                value={countryCode}
-              />
-              <TextInput
-                style={[styles.input, { flex: 1 }]}
-                placeholder='Mobile Number'
-                keyboardType='numeric'
-                placeholderTextColor={Colors.gray}
-                value={phoneNumber}
-                onChangeText={setPhoneNumber}
-              />
-            </View>
-
-            <Link href={'/login'} asChild replace>
-              <TouchableOpacity>
-                <Text style={defaultStyles.textLink}>Already have an account? Log in</Text>
-              </TouchableOpacity>
-            </Link>
-
-            <View style={{ flex: 1 }}></View>
-
-            <TouchableOpacity
-              style={[
-                defaultStyles.pillButton,
-                phoneNumber != '' ? styles.enabled : styles.disabled,
-                { marginBottom: 20 }
-              ]}
-              onPress={onSignup}
-            >
-              <Text style={defaultStyles.buttonText}>Sign up</Text>
-            </TouchableOpacity>
-          </View>
-        </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
-    );
+    //   router.push({ pathname: '/verify/[phone]', params: { phone: fullPhoneNumber } });
+    // } catch (error) {
+    //   console.error('Error signing up:', error);
+    // }
   };
 
-  const styles = StyleSheet.create({
-    inputContainer: {
-      flexDirection: 'row',
-      marginVertical: 40,
-    },
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior='padding' keyboardVerticalOffset={keyboardVerticalOffset}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={defaultStyles.container}>
+          <Text style={defaultStyles.header}>Let's get started!</Text>
+          <Text style={defaultStyles.descriptionText}>Enter your phone number to start. We will send you confirmation code shortly!</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder='Country Code'
+              placeholderTextColor={Colors.dark}
+              value={countryCode}
+            />
+            <TextInput
+              style={[styles.input, { flex: 1 }]}
+              placeholder='Mobile Number'
+              keyboardType='numeric'
+              placeholderTextColor={Colors.gray}
+              value={phoneNumber}
+              onChangeText={setPhoneNumber}
+            />
+          </View>
 
-    input: {
-      backgroundColor: Colors.lightGray,
-      padding: 20,
-      borderRadius: 16,
-      fontSize: 20,
-      marginRight: 10,
-    },
+          <Link href={'/login'} asChild replace>
+            <TouchableOpacity>
+              <Text style={defaultStyles.textLink}>Already have an account? Log in</Text>
+            </TouchableOpacity>
+          </Link>
 
-    enabled: {
-      backgroundColor: Colors.primary,
-    },
+          <View style={{ flex: 1 }}></View>
 
-    disabled: {
-      backgroundColor: Colors.primaryMuted,
-    }
+          <TouchableOpacity
+            style={[
+              defaultStyles.pillButton,
+              phoneNumber != '' ? styles.enabled : styles.disabled,
+              { marginBottom: 20 }
+            ]}
+            onPress={onSignup}
+          >
+            <Text style={defaultStyles.buttonText}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
 
-  });
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    marginVertical: 40,
+  },
 
-  export default Page;
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  }
+
+});
+
+export default Page;
--- app/verify/[phone].tsx
+++ app/verify/[phone].tsx
@@ -1,9 +1,32 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocalSearchParams } from 'expo-router';
+import { useSignIn, useSignUp } from '@clerk/clerk-expo';
+
 
 const Page = () => {
     const { phone, signin } = useLocalSearchParams<{ phone: string, signin: string; }>();
+    const [code, setCode] = useState('');
+    const { signIn } = useSignIn();
+    const { signUp, setActive } = useSignUp();
+
+    useEffect(() => {
+        if (code.length === 6) {
+            if (signin === 'true') {
+                verifySignIn();
+            } else {
+                verifyCode();
+            }
+        }
+    }, [code]);
+
+    const verifyCode = async () => {
+
+    };
+
+    const verifySignIn = async () => { };
+
+
     return (
         <View>
             <Text>Page</Text>
